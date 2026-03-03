09323af06de8b009ddbe6dffa9eedd752c18f807
MehtabWazir34 <mehtabwazir34@gmail.com>
2026-03-04 03:54:48 +0500

    setting1234

--- BackEnd/api/server.js
+++ BackEnd/api/server.js
@@ -47,9 +47,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Internal Server Error" });
 });
 
-const PORT = process.env.PORT || 3400;
-app.listen(PORT, () => {
-  console.log(`Local server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3400;
+// app.listen(PORT, () => {
+//   console.log(`Local server running on port ${PORT}`);
+// });
 
 export default app;
